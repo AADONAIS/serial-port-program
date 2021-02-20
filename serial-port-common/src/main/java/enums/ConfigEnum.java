e2493303686c5e6eaf4a7c7b81e6e6960994c3e0
Leon <job@liaojianing.com>
2021-02-20 18:26:02 +0800

    构建公共模块_添加公共枚举

---
+++ serial-port-common/src/main/java/enums/ConfigEnum.java
@@ -0,0 +1,18 @@
+package enums;
+
+/**
+ * ConfigEnum
+ *
+ * @author Leon
+ * @version 1.0
+ * @date 2021/2/20 18:25
+ **/
+public abstract class ConfigEnum {
+
+    public static final int PARITY_NONE = 0;
+    public static final int PARITY_ODD = 1;
+    public static final int PARITY_EVEN = 2;
+    public static final int PARITY_MARK = 3;
+    public static final int PARITY_SPACE = 4;
+
+}
