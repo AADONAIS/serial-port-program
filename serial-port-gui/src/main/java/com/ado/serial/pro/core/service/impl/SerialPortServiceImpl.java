7c997de11facf220598e8baa39966aa925a5c541
Leon <job@liaojianing.com>
2021-02-23 10:10:46 +0800

    fixup

--- serial-port-gui/src/main/java/com/ado/serial/pro/core/service/impl/SerialPortServiceImpl.java
+++ serial-port-gui/src/main/java/com/ado/serial/pro/core/service/impl/SerialPortServiceImpl.java
@@ -1,11 +1,22 @@
 package com.ado.serial.pro.core.service.impl;
 
 import com.ado.serial.pro.core.service.SerialPortService;
+import enums.SerialPortConfigEnum;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class SerialPortServiceImpl implements SerialPortService {
 
+
+    private static final List<SerialPortConfigEnum> keys = new ArrayList<SerialPortConfigEnum>() {{
+        add(SerialPortConfigEnum.PARITY_CHECK);
+        add(SerialPortConfigEnum.SERIAL_NUMBER);
+        add(SerialPortConfigEnum.BAUD_RATE);
+        add(SerialPortConfigEnum.STOP_BITS);
+        add(SerialPortConfigEnum.DATA_BITS);
+    }};
 
     public boolean dealData(Map<String, String> map) {
         return false;
